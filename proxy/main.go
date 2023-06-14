ce09e99218712c55b4ea5bf7794c7479f454d433
devofficer <sompaqueen47@gmail.com>
2023-06-14 19:07:11 -0400

    feat: implement jwt authentication for proxy cconnections

--- proxy/main.go
+++ proxy/main.go
@@ -1,17 +1,48 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"strings"
+
+	"go-fiber-auth/utilities"
 
 	"github.com/elazarl/goproxy"
 )
 
 func main() {
 	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = false
 
-	proxy.OnRequest().HandleConnectFunc(
-		func(host string, ctx *goproxy.ProxyCtx) (*goproxy.ConnectAction, string) {
-			return nil, host
+	proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
+	proxy.OnRequest().DoFunc(
+		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+			proxyAuthToken := r.Header.Get("Proxy-Authorization")
+
+			if proxyAuthToken == "" {
+				return r, goproxy.NewResponse(r,
+					goproxy.ContentTypeText, http.StatusForbidden,
+					"Don't waste your time!")
+			}
+
+			trimmedToken := strings.TrimSpace(proxyAuthToken)
+
+			if trimmedToken == "" {
+				return r, goproxy.NewResponse(r,
+					goproxy.ContentTypeText, http.StatusForbidden,
+					"Don't waste your time!")
+			}
+
+			claims, parsingError := utilities.ParseClaims(trimmedToken)
+			if parsingError != nil {
+				return r, goproxy.NewResponse(r,
+					goproxy.ContentTypeText, http.StatusForbidden,
+					"Not authorized!")
+			}
+
+			log.Println(claims)
+
+			return r, nil
 		})
 
 	http.ListenAndServe(":8080", proxy)
